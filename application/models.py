f340b03a99862bcf6f212c44c6b7ee7f4fbddb20
matthew-shaw <matthew.shaw@digital.landregistry.gov.uk>
2016-09-12 23:31:25 +0100

    memoise external api calls

--- application/models.py
+++ application/models.py
@@ -1,3 +1,4 @@
+from application import cache
 import json
 import requests
 
@@ -8,36 +9,40 @@ class Healthcare(object):
         super(Healthcare, self).__init__()
         self.base_url = 'https://data.gov.uk/data/api/service/health'
 
-    def find_by_name(self, type, name):
+    @cache.memoize(timeout=3600)
+    def find_by_name(self, service, name):
         url = '{0}/{1}/organisation_name?organisation_name={2}'
-        response = requests.get(url.format(self.base_url, type, name))
+        response = requests.get(url.format(self.base_url, service, name))
         if response.status_code != requests.codes.ok:
             response.raise_for_status()
         else:
             result = json.loads(response.text)
         return result
 
-    def find_by_postcode(self, type, postcode):
+    @cache.memoize(timeout=3600)
+    def find_by_postcode(self, service, postcode):
         url = '{0}/{1}/partial_postcode?partial_postcode={2}'
-        response = requests.get(url.format(self.base_url, type, postcode))
+        response = requests.get(url.format(self.base_url, service, postcode))
         if response.status_code != requests.codes.ok:
             response.raise_for_status()
         else:
             result = json.loads(response.text)
         return result
 
-    def find_by_city(self, type, city):
+    @cache.memoize(timeout=3600)
+    def find_by_city(self, service, city):
         url = '{0}/{1}?city={2}'
-        response = requests.get(url.format(self.base_url, type, city))
+        response = requests.get(url.format(self.base_url, service, city))
         if response.status_code != requests.codes.ok:
             response.raise_for_status()
         else:
             result = json.loads(response.text)
         return result
 
-    def find_by_county(self, type, name):
+    @cache.memoize(timeout=3600)
+    def find_by_county(self, service, name):
         url = '{0}/{1}?county={2}'
-        response = requests.get(url.format(self.base_url, type, name))
+        response = requests.get(url.format(self.base_url, service, name))
         if response.status_code != requests.codes.ok:
             response.raise_for_status()
         else:
